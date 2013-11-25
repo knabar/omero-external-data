315c872be43c42cf392576d5602f9ce335f59ece
Andreas Knab <knabar@gmail.com>
2013-11-25 14:23:44 +0100

    Use gateway methods

--- omero_external_data/urls.py
+++ omero_external_data/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('django.views.generic.simple',
         views.load_datasource, name='omero_external_data_load_datasource'),
     url(r'^datasource/(?P<obj_dtype>\w+)/(?P<obj_id>[0-9]+)/delete/$',
         views.delete_datasource, name='omero_external_data_delete_datasource'),
-    url(r'^datasource/datasource_type_form/$',
+    url(r'^(?P<obj_dtype>\w+)/(?P<obj_id>[0-9]+)/datasource/form/$',
         views.render_form, name='omero_external_data_datasource_type_form'),
 
  )
--- omero_external_data/views.py
+++ omero_external_data/views.py
@@ -6,41 +6,7 @@ from django import forms
 from omeroweb.webclient.decorators import login_required, render_response
 from omero.gateway import TermAnnotationWrapper
 
-import settings
-from mocked import get_datasource_types, get_datasource_type
-
-
-def fetch_annotations(conn, obj_dtype, obj_id):
-    """
-    Return the first available annotation for the given object in this app's
-    namespace, or None if no annotation exists.
-    """
-    annotations = conn.getAnnotationLinks(
-        obj_dtype, [obj_id], ns=settings.ANNOTATION_NAMESPACE)
-    for annotation in annotations:
-        yield annotation.getAnnotation()
-
-
-def fetch_datasources(conn, obj_dtype, obj_id):
-    for annotation in fetch_annotations(conn, obj_dtype, obj_id):
-        datasource = json.loads(annotation.getValue())
-        datasource["id"] = annotation.id
-        yield datasource
-
-
-def store_datasource(conn, obj_dtype, obj_id, datasource):
-    obj = conn.getObject(str(obj_dtype), obj_id)
-    TermAnnotationWrapper.createAndLink(
-        obj, settings.ANNOTATION_NAMESPACE, val=datasource)
-
-
-def remove_datasource(conn, obj_dtype, obj_id, datasource_id):
-    handle = conn.deleteObjects('/Annotation', [datasource_id])
-    try:
-        conn._waitOnCmd(handle)
-        return True
-    finally:
-        handle.close()
+from omero.ds.core import DataSource
 
 
 @login_required()
@@ -54,18 +20,36 @@ def index(request, obj_dtype, obj_id, conn=None, **_kwargs):
 
         if request.GET.has_key('add_datasource'):
 
-            datasource = json.dumps({
-                'type': request.POST.get('datasource_type'),
-                'inputs': {
-                    'name': 'value'
-                },
-            })
+            datasource_type = next(
+                dstype for dstype in conn.listDatasourceTypes(obj_dtype)
+                if dstype.ds_name == request.POST.get('datasource_type')
+                )
 
-            store_datasource(conn, obj_dtype, obj_id, datasource)
+            form = generate_form(datasource_type, request.POST)
 
-    datasources = fetch_datasources(conn, obj_dtype, obj_id)
+            if form.is_valid():
 
-    datasource_types = get_datasource_types(obj_dtype)
+                datasource = DataSource({
+                    'name': form.cleaned_data['name'],
+                    'label': form.cleaned_data['label'],
+                    'type': request.POST.get('datasource_type'),
+                    'inputs': [
+                        {
+                            'name': field['name'],
+                            'value': form.cleaned_data[field['name']],
+                        }
+                        for field in datasource_type['inputs']
+                    ],
+                })
+                conn.attachDatasource(datasource, obj_dtype, obj_id)
+
+            else:
+
+                return dict(bad=True, errs=form.errors)
+
+    datasources = conn.listDatasources(obj_dtype, obj_id)
+
+    datasource_types = conn.listDatasourceTypes(obj_dtype)
 
     context = {
         'obj_dtype': obj_dtype,
@@ -82,14 +66,15 @@ def index(request, obj_dtype, obj_id, conn=None, **_kwargs):
 @login_required()
 @render_response()
 def load_datasource(request, obj_dtype, obj_id, conn=None, **_kwargs):
-    return "Test for %s %s %s" % (obj_dtype, obj_id, request.GET.get('datasource'))
+    result = conn.openDatasourceCursor(int(request.GET.get('datasource'))).read()
+    return HttpResponse(content=result)
 
 
 @login_required()
 @render_response()
 def delete_datasource(request, obj_dtype, obj_id, conn=None, **_kwargs):
     try:
-        bad = not remove_datasource(conn,  obj_dtype, obj_id, int(request.GET.get('datasource')))
+        bad = not conn.removeDatasource(request.GET.get('datasource'))
         errs = 'Could not remove data source' if bad else None
     except Exception, ex:
         bad = True
@@ -97,36 +82,37 @@ def delete_datasource(request, obj_dtype, obj_id, conn=None, **_kwargs):
     return dict(bad=bad, errs=errs)
 
 
-@login_required()
-@render_response()
-def render_form(request, conn=None, **_kwargs):
-
-    datasource_type = get_datasource_type(request.GET.get('datasource_type'))
+def generate_form(datasource_type, form_data=None):
 
     class Form(forms.Form):
-        pass
+        name = forms.CharField(label='Name', required=True)
+        label = forms.CharField(label='Label', required=True)
 
-    form = Form()
+    form = Form(form_data)
 
-    for field in datasource_type['fields']:
+    for field in datasource_type['inputs']:
         form.fields[field['name']] = forms.CharField(
             label=field['label'],
             initial=field['default'],
             required=field['required'],
             )
 
+    return form
+
+
+@login_required()
+@render_response()
+def render_form(request, obj_dtype, obj_id, conn=None, **_kwargs):
+
+    datasource_type = next(
+        dstype for dstype in conn.listDatasourceTypes(obj_dtype)
+        if dstype.ds_name == request.GET.get('datasource_type')
+        )
+
+    form = generate_form(datasource_type)
+
     context = {
         'form': form,
         'template': 'omero_external_data/datasource_type_form.html'
     }
     return context
-
-
-
-                #{
-                #    'name': 'url',
-                #    'label': 'URL',
-                #    'field_type': 'text',
-                #    'default': None,
-                #    'required': True,
-                #},
